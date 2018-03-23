5861967e98af4e94e3e0e6012e25954121c00594
katietieu <25238141+katietieu@users.noreply.github.com>
2018-03-22 18:37:02 -0600

    Check for java in jar - not working!

--- Iteration2.java
+++ Iteration2.java
@@ -1,20 +1,25 @@
-
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.jar.*;
+import java.util.zip.ZipEntry;
 
 import org.eclipse.jdt.core.dom.*;
 
-public class Iteration1 {
+public class Iteration2 {
 	public static ArrayList<String> listOfDirs = new ArrayList<String>();
 	public static File[] javaFiles;
 	public int count_dec;
 	public int count_ref;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		int argsLength = args.length;
 		
 		// Throw error if invalid amount of arguments and terminate
@@ -27,13 +32,16 @@ public class Iteration1 {
 		}
 		
 		// if correct amount of arguments then create new instance and start work
-		Iteration1 it1 = new Iteration1(args[0]);
+		Iteration2 it2 = new Iteration2(args[0]);
 		System.out.println("Current List of Dirs: " + listOfDirs + "\n");
 
 		for(int i = 0; i <= listOfDirs.size() - 1; i++ ) {
 			javaFiles = fileFinder(listOfDirs.get(i));
 			
-			for (File javaFile : it1.javaFiles) {
+			for (File javaFile : it2.javaFiles) {
+//				if (javaFile.getName().endsWith(".jar")) {
+//					javaFiles = getJarEntries(new JarFile(args[0] + "/" + javaFile.getName()));
+//				}
 				String sourceCode;
 				
 				// Try to read the contents of file, if error occurs, skip and go to next file.
@@ -44,11 +52,11 @@ public class Iteration1 {
 				}
 				
 				// if contents successfully read then parse the contents
-				it1.parse(sourceCode);
+				it2.parse(sourceCode);
 			}
 		}
 		// print result
-		it1.print();
+		it2.print();
 		
 		return;
 	}
@@ -58,7 +66,7 @@ public class Iteration1 {
 	 * Takes given pathName
 	 *  
 	 */
-	public Iteration1(String pathName) {
+	public Iteration2(String pathName) {
 		listOfDirs.add(pathName);			//Might need to change this.. does 
 		File[] files = new File(pathName).listFiles();
 		pathFinder(files);
@@ -98,11 +106,60 @@ public class Iteration1 {
         });
     }
 	
+	/**
+	 * Adds java files from a jar file to the javaFiles array
+	 * @param absolute pathname to jar file
+	 * @throws IOException 
+	 */
+	private static File[] getJarEntries(JarFile jarFile) throws IOException {
+		ArrayList<File> javaFilesCopy = new ArrayList<File>(Arrays.asList(javaFiles));	// Create new ArrayList for java files so we can append to it
+		Enumeration<JarEntry> jarEnum = jarFile.entries();
+		while (jarEnum.hasMoreElements()) {		// Go through each entry
+			ZipEntry ze = checkIfJava(jarFile, jarEnum.nextElement());				// Check if java
+			
+			if (ze != null) {					// If is java, create InputStream and convert file to String
+				File tempFile = File.createTempFile(jarEnum.nextElement().getName(), ".java");	// Create temp file with same name as file in jar
+				tempFile.deleteOnExit();
+				
+				InputStream inputStream = jarFile.getInputStream(ze);
+				FileOutputStream out = new FileOutputStream(tempFile);
+				byte[] buffer = new byte[1024];
+				int length;
+				while ((length = inputStream.read(buffer)) != -1) {
+				    out.write(buffer, 0, length);
+				}
+				javaFilesCopy.add(tempFile);
+//				System.out.println(javaFilesCopy);
+			}
+		}
+		File[] javaFiles = new File[javaFilesCopy.size()];
+		javaFiles = javaFilesCopy.toArray(javaFiles);
+		return javaFiles;
+		
+	}
 	
+	/**
+	 * Checks if the jar entry in question ends in .java
+	 * @param jarFile the jar file being checked
+	 * @param jarEntry the JarEntry to check
+	 * @return ZipEntry of jar entry if it is a java file
+	 */
+	// Change method name?
+	private static ZipEntry checkIfJava(JarFile jarFile, Object jarEntry) {
+		JarEntry entry = (JarEntry)jarEntry;
+		String name = entry.getName();
+		if (name.endsWith(".java")) {
+			ZipEntry ze = jarFile.getEntry(name);
+			return ze;
+		}
+		else { 
+			return null;
+		}
+	}
+
 	/**
 	 * Parses given source code and increments count_dec and count_ref if necessary.
 	 */
-	@SuppressWarnings("deprecation")
 	public void parse(String sourceCode) {
 		ASTParser parser = ASTParser.newParser(AST.JLS8);
 
